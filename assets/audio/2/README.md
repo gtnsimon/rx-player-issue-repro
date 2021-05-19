805e5da253ce84f2b4dc52e06ade5ecb03090cd5
Gaëtan Simon <gaetan.simon@jack.agency>
2021-05-19 13:53:02 +0200

    repro: working example with rx-player 3.23.1

---
+++ assets/audio/2/README.md
@@ -0,0 +1,5 @@
+# ffmpeg command
+
+```bash
+ffmpeg -y -hide_banner -vsync passthrough -i /sources/audio/2.wav -stdin -map 0:a:0 -c:a libfdk_aac -b:a 320k -seg_duration 5 -use_timeline 0 -use_template 1 -hls_playlist 1 -single_file 1 -single_file_name stream.$ext$ -f dash /assets/audio/2/manifest.mpd
+```
---
+++ main.js
@@ -0,0 +1,77 @@
+// load styles for demo
+import './styles.scss'
+
+import RxPlayer from 'rx-player/minimal'
+import { DASH } from 'rx-player/features'
+
+// keep reference to rx-player element to dispose it later
+let oldRxPlayer
+
+/**
+ * @param {'audio' | 'video'} type
+ * @param {string} url Manifest URL
+ */
+function createDashMediaElement (type, url) {
+  const mediaElement = document.createElement(type)
+  const rxPlayer = new RxPlayer({ videoElement: mediaElement })
+
+  mediaElement.id = type
+  mediaElement.controls = true
+
+  rxPlayer.loadVideo({
+    url,
+    transport: 'dash',
+  })
+
+  return [ rxPlayer, mediaElement ]
+}
+
+/**
+ * Destroy media element in media preview
+ * @param {RxPlayer} rxPlayer
+ */
+function disposeRxPlayer (rxPlayer) {
+  if (!rxPlayer) {
+    return
+  }
+
+  rxPlayer.dispose()
+  rxPlayer = undefined
+}
+
+/**
+ * Set media element to play
+ *
+ * @param {HTMLDivElement} mediaPreview
+ * @param {string} value
+ */
+function setMediaPreviewElement (mediaPreview, value) {
+  disposeRxPlayer(oldRxPlayer)
+
+  const [ type, url ] = value.split(',', 2)
+  const [ rxPlayer, mediaElement ] = createDashMediaElement(type, url)
+
+  mediaPreview.innerHTML = ''
+  mediaPreview.appendChild(mediaElement)
+
+  oldRxPlayer = rxPlayer
+}
+
+// add DASH feature to rx-player
+RxPlayer.addFeatures([ DASH ])
+
+// load media element when document is ready
+document.addEventListener('DOMContentLoaded', () => {
+  // get media selector
+  const mediaSelector = document.querySelector('#mediaSelector')
+  // get media preview
+  const mediaPreview = document.querySelector('#mediaPreview')
+
+  // listen for media changes
+  mediaSelector.addEventListener('change', (e) => {
+    setMediaPreviewElement(mediaPreview, e.target.value)
+  })
+
+  // set initial media element
+  setMediaPreviewElement(mediaPreview, mediaSelector.value)
+})
